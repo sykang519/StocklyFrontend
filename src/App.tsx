9541d90686b31c825152af24fd21b4d732e011d5
sykang519 <sykang@swrd.cc>
2024-10-02 11:44:03 +0900

    Add: 메인페이지 파일 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
+import MainPage from './pages/MainPage';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
         <BrowserRouter>
           <Routes>
             <Route path="/login" element={<LoginPage />} />
+            <Route path="/main" element={<MainPage />} />
           </Routes>
         </BrowserRouter>
       }
---
+++ src/pages/MainPage.tsx
@@ -0,0 +1,11 @@
+
+
+function MainPage() {
+  return (
+    <>
+     메인페이지
+    </>
+  );
+}
+
+export default MainPage;

36edade927b7c91ea5d755a6facf1243dc2d42f7
sykang519 <sykang@swrd.cc>
2024-10-02 11:08:22 +0900

    init

---
+++ src/components/SignIn.tsx
@@ -0,0 +1,24 @@
+function SignIn() {
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <div className="text-[50px] mb-[150px]">로그인</div>
+      <form className="flex flex-col justify-center items-center">
+        <input
+          className="w-[575px] h-[80px] m-[15px] p-[20px] rounded-[7px] shadow"
+          placeholder="이메일"
+          required
+        ></input>
+        <input
+          className="w-[575px] h-[80px] m-[15px] p-[20px] rounded-[7px] shadow"
+          placeholder="비밀번호"
+          required
+        ></input>
+        <button className="w-[300px] h-[65px] mt-[100px] text-[28px] bg-MainBlue rounded-[7px]" type="submit">
+          로그인
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default SignIn;
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+    colors: {
+      'MainBlue' : '#3182F6',
+    }
+  },
+  plugins: [],
+}

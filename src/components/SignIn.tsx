a7f718916e454fafd300922f115f94d4eca3d662
sykang519 <sykang@swrd.cc>
2024-10-08 16:29:17 +0900

    Chore: 로그인 버튼 페이지 연결

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -1,4 +1,11 @@
+import { useNavigate } from 'react-router-dom';
+
 function SignIn() {
+  const navigate = useNavigate();
+
+  const gotoMain = () => {
+    navigate('/main');
+  }
   return (
     <div className="flex flex-col justify-center items-center">
       <div className="text-[50px] mb-[150px]">로그인</div>
@@ -13,7 +20,11 @@ function SignIn() {
           placeholder="비밀번호"
           required
         ></input>
-        <button className="w-[300px] h-[65px] mt-[100px] text-[28px] bg-MainBlue text-white rounded-[7px]" type="submit">
+        <button
+          className="w-[300px] h-[65px] mt-[100px] text-[28px] bg-MainBlue text-white rounded-[7px]"
+          type="submit"
+          onClick={gotoMain}
+        >
           로그인
         </button>
       </form>

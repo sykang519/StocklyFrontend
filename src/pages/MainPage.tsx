b8f381d2d9c34ea597067218b2738683bcecce16
sykang519 <sykang@swrd.cc>
2024-10-14 03:05:53 +0000

    chore: 메뉴바 우측 상단에 고정되도록 수정

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,11 +1,12 @@
 import TopNavBar from '../components/TopNavBar';
 import StockChartList from '../components/StockChartList';
+import useDrawerStore from '../zustand/MenuBarStore';
 
 function MainPage() {
-
+  const {openDrawer} = useDrawerStore();
   return (
     <>
-      <div className="w-[100%] h-[100vh]">
+      <div className={`w-[100%] h-[100vh] transition-all duration-300 ${openDrawer ? 'mr-[370px]' : 'mr-[70px]'}`}>
         <TopNavBar />
         <div className="flex justify-center ">
           <div className="w-[95%] min-w-[800px] max-w-[1500px]">

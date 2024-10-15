bce15ea9598151c22254be17a6ebc7cf1639a79d
sykang519 <sykang@swrd.cc>
2024-10-15 15:23:34 +0900

    chore: 주문 컴포넌트 css 업데이트

--- src/components/orderContents/Order.tsx
+++ src/components/orderContents/Order.tsx
@@ -4,17 +4,20 @@ import { useState } from 'react';
 
 function Order() {
   const [content, setContent] = useState('buy');
+  const [position, setPosition] = useState(0); // 시작 위치
 
   const handleClickBuy = () => {
     setContent('buy');
+    setPosition(0); // 자식 div를 원래 위치로 이동
   };
 
   const handleClickSell = () => {
     setContent('sell');
+    setPosition(100); // 자식 div를 오른쪽으로 이동
   };
 
   return (
-    <div>
+    <div className="w-full overflow-hidden">
       <div className="text-[17px] font-bold p-[15px]">주문하기</div>
       <div className="flex px-[15px]">
         <button
@@ -30,8 +33,11 @@ function Order() {
           매도
         </button>
       </div>
-      <div>
-        {content==="buy" ? <Buy/> : <Sell/>}
+      <div
+        className={`flex w-[200%] transform transition-transform duration-300 ${position === 0 ? 'translate-x-0' : '-translate-x-1/2'}`}
+      >
+        <Buy />
+        <Sell />
       </div>
     </div>
   );

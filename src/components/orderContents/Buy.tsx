76db93913f1701d72f0e7d1f2bd5bc8c97eef29f
sykang519 <sykang@swrd.cc>
2024-10-16 11:26:35 +0900

    chore: css 개선

--- src/components/orderContents/Buy.tsx
+++ src/components/orderContents/Buy.tsx
@@ -5,6 +5,10 @@ import { useState, useEffect } from 'react';
 function Buy() {
   const [isDisabled, setIsDisabled] = useState(true);
 
+  const [purchase, setPurchase] = useState('limit');
+  //limit : 지정가
+  //market : 시장가
+
   const [price, setPrice] = useState('');
   const plusPrice = () => {
     setPrice(String(Number(price) + 1));
@@ -22,7 +26,7 @@ function Buy() {
   };
 
   useEffect(() => {
-    if ((Number(price) > 0 && price[0]!=="0") && (Number(quantity) > 0 && quantity[0]!=="0")) {
+    if ((Number(price) > 0 && price[0] !== '0') && (Number(quantity) > 0 && quantity[0] !== '0')) {
       setIsDisabled(false);
     } else {
       setIsDisabled(true);
@@ -37,10 +41,25 @@ function Buy() {
   return (
     <div className="flex flex-col justify-center items-center mt-[20px] w-full">
       <div className="flex flex-column  w-[90%] my-[5px]">
-        <div className="w-[30%] h-[33px] ">구매 가격</div>
-        <div className="bg-gray w-[70%] rounded-[7px] flex">
-          <button className="w-[50%] text-[13px] flex justify-center items-center">지정가</button>
-          <button className="w-[50%] text-[13px] flex justify-center items-center">시장가</button>
+        <div className="w-[30%] h-[33px] content-center ">구매 가격</div>
+        <div className="bg-gray w-[70%] rounded-[7px] flex items-center relative ">
+          <div
+            className={`bg-white w-[50%] absolute rounded-[5px] z-1 text-white transition-transform duration-300 ease-in-out  ${purchase === "limit" ? "translate-x-[5%]" : "translate-x-[95%]"}`}
+          >
+            .
+          </div>
+          <button
+            className="w-[50%] text-[13px] flex justify-center items-center bg-none z-10 content-center"
+            onClick={() => setPurchase('limit')}
+          >
+            지정가
+          </button>
+          <button
+            className="w-[50%] text-[13px] flex justify-center items-center bg-none z-10 content-center"
+            onClick={() => setPurchase('market')}
+          >
+            시장가
+          </button>
         </div>
       </div>
       <div className="flex flex-column justify-end w-[90%] my-[5px]">
--- src/components/stockDetails/TopContent.tsx
+++ src/components/stockDetails/TopContent.tsx
@@ -2,6 +2,8 @@ import like from '../../assets/icons/like.svg';
 import like_hover from '../../assets/icons/like_hover.svg';
 import alert from '../../assets/icons/alert.svg';
 import alert_hover from '../../assets/icons/alert_hover.svg';
+import like2 from '../../assets/icons/like2.svg';
+import like2_hover from '../../assets/icons/like2_hover.svg';
 import { useState, useEffect } from 'react';
 import * as React from 'react';
 import Menu from '@mui/material/Menu';
@@ -9,8 +11,9 @@ import Menu from '@mui/material/Menu';
 function TopContent() {
   const [likeSrc, setLikeSrc] = useState(like);
   const [alertSrc, setAlertSrc] = useState(alert);
-  const [price, setPrice] = useState("");
+  const [price, setPrice] = useState('');
   const [isDisabled, setIsDisabled] = useState(true);
+  const [isLike, setIsLike] = useState(false);
 
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
   const open = Boolean(anchorEl);
@@ -22,11 +25,14 @@ function TopContent() {
     setAnchorEl(null);
   };
 
+  const handleLike = () =>{
+    setIsLike(!isLike);
+  }
+
   useEffect(() => {
-    if (Number(price) > 0 && price[0]!=='0'){
+    if (Number(price) > 0 && price[0] !== '0') {
       setIsDisabled(false);
-    }
-    else{
+    } else {
       setIsDisabled(true);
     }
   }, [price]);
@@ -83,25 +89,36 @@ function TopContent() {
               얼마일 때 알려드릴까요?
             </div>
             <div className="border border-MainBlue mx-[20px] rounded-[5px] flex justify-between items-center">
-              <input className="w-[90%] outline-none px-[5px] m-[2px]" placeholder="가격을 입력하세요" value={price} onChange={(e)=>setPrice(e.target.value)}/>
+              <input
+                className="w-[90%] outline-none px-[5px] m-[2px]"
+                placeholder="가격을 입력하세요"
+                value={price}
+                onChange={(e) => setPrice(e.target.value)}
+              />
               <p className="w-[10%]">원</p>
             </div>
           </div>
           <div>
             <div className="w-full flex justify-end ">
-              <button className=" m-[20px] bg-MainBlue text-white w-[60px] h-[30px] text-[13px] rounded-[3px]" disabled={isDisabled}>
+              <button
+                className=" m-[20px] bg-MainBlue text-white w-[60px] h-[30px] text-[13px] rounded-[3px]"
+                disabled={isDisabled}
+              >
                 알림 받기
               </button>
             </div>
           </div>
         </Menu>
-        <img
-          src={likeSrc}
-          className="mr-[30px] cursor-pointer w-[48.5px] h-[48.5px]"
-          alt="좋아요"
-          onMouseEnter={() => setLikeSrc(like_hover)} // hover 상태일 때
-          onMouseLeave={() => setLikeSrc(like)} // 기본 상태로 복귀
-        />
+        <div>
+          <img
+            src={likeSrc}
+            className="mr-[30px] cursor-pointer w-[48.5px] h-[48.5px]"
+            alt="좋아요"
+            onMouseEnter={() => setLikeSrc(isLike ? like2_hover : like_hover)} // hover 상태일 때
+            onMouseLeave={() => setLikeSrc(isLike ? like2 : like)} // 기본 상태로 복귀
+            onClick={handleLike}
+          />
+        </div>
       </div>
     </div>
   );

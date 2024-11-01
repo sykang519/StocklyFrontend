0511d3708cdc1e10370d99f346eb89fb13da028e
sykang519 <sykang@swrd.cc>
2024-11-01 17:03:19 +0900

    fix: 새로고침시에도 로그인 상태 유지하게 수정

--- src/zustand/UserStore.tsx
+++ src/zustand/UserStore.tsx
@@ -1,17 +1,25 @@
 import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
 
 interface UserState {
   isLoggedin: boolean;
   email: string;
-  setUserState:(login: boolean, name: string) => void;
+  setUserState: (login: boolean, name: string) => void;
 }
 
-const useUserStore = create<UserState>((set) => ({
-  isLoggedin: false,
-  email: '',
-  setUserState: (login: boolean, name: string) => {
-    set({isLoggedin: login, email:name})
-  },
-}));
+const useUserStore = create(
+  persist<UserState>(
+    (set) => ({
+      isLoggedin: false,
+      email: '',
+      setUserState: (login: boolean, name: string) => {
+        set({ isLoggedin: login, email: name });
+      },
+    }),
+    {
+      name: 'userStorage',
+    },
+  ),
+);
 
 export default useUserStore;

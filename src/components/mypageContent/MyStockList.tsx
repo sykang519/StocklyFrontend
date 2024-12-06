e0fae5b8f4101e43436f6cc93105298598eb483d
sykang519 <sykang@swrd.cc>
2024-12-06 06:44:05 +0000

    feat: 보유 주식 조회 api 연동

--- src/components/mypageContent/MyStockList.tsx
+++ src/components/mypageContent/MyStockList.tsx
@@ -1,9 +1,39 @@
+import { useState, useEffect } from 'react';
+
+interface stockListItem {
+  symbol: string;
+  name: string;
+  volume: number;
+  purchase_price: number;
+  current_price: number;
+  price_difference: number;
+  roi: number;
+  total_stock_prices: number;
+}
+
 function MyStockList() {
-  const stocks = [
-    { company_name: '삼성전자', volume: 2, price: 60000, rate: 2.5, rate_price: 3500 },
-    { company_name: '삼성전자', volume: 2, price: 60000, rate: -2.5, rate_price: -3500 },
-    { company_name: '삼성전자111', volume: 2, price: 60000, rate: 2.5, rate_price: 3500 },
-  ];
+  const [stocklist, setStockList] = useState<stockListItem[]>([]);
+
+  useEffect(() => {
+    fetch('http://localhost:30082/api/v1/invests/roi/latest', {
+      method: 'GET',
+      headers: { 'Content-Type': 'application/json' },
+      credentials: 'include',
+    })
+      .then((res) => {
+        if (!res.ok) {
+          console.log('네트워크 응답이 올바르지 않습니다.');
+        }
+        return res.json();
+      })
+      .then((fetchedData) => {
+        setStockList(fetchedData.data);
+      })
+      .catch((error) => {
+        console.error('데이터를 가져오는 중 오류가 발생하였습니다:', error);
+      });
+  }, []);
+
   return (
     <div className="flex flex-col justify-center items-center overflow-scroll m-[5px]">
       <table className="w-full">
@@ -17,22 +47,20 @@ function MyStockList() {
           </tr>
         </thead>
         <tbody>
-          {stocks.map((data, index) => (
+          {stocklist.map((data, index) => (
             <tr key={index} className="rounded-[5px] hover:bg-Bg-gray cursor-pointer">
-              <td className="text-left py-[10px] text-chart-font px-2 text-[17px]">{data.company_name}</td>
+              <td className="text-left py-[10px] text-chart-font px-2 text-[17px]">{data.name}</td>
               <td className="text-right py-[10px] text-chart-font text-[17px]">
-                {data.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')} 원
+                {data.purchase_price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')} 원
               </td>
-              <td
-                className={`text-right py-[10px] text-chart-font text-[17px]`}
-              >
-                {data.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}원
+              <td className={`text-right py-[10px] text-chart-font text-[17px]`}>
+                {data.current_price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}원
               </td>
               <td className="text-right py-[10px] text-chart-font text-[17px]">{data.volume}주</td>
               <td className="text-right py-[10px] text-chart-font text-[17px] px-2">
-                <p>{(data.price * data.volume).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')} 원</p>
-                <p className={`text-[13px] ${data.rate>0 ? "text-up" : "text-down"}`}>
-                  {data.rate_price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}원 ({data.rate})% 원
+                <p>{(data.total_stock_prices).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')} 원</p>
+                <p className={`text-[13px] ${data.roi > 0 ? 'text-up' : 'text-down'}`}>
+                  {data.price_difference.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}원 ({data.roi})% 원
                 </p>
               </td>
             </tr>
